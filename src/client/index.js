cdae8861cb4ad80a45f84e56ca3789ccb9d0a03b
tim doyle <coder.td@gmail.com>
2019-12-27 14:59:59 -0500

    Router/Switch setup.

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Route, Link, BrowserRouter as Router, Switch
+} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Users from './pages/Users';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
